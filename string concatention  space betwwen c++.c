304ba41869e5d1de44bb68dcc0b61bab932f1655
RoshniSingh2068 <roshnisingh1228@gmail.com>
2024-08-26 10:52:42 +0530

    Create string concatention  space betwwen c++.c

---
+++ string concatention  space betwwen c++.c
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main (){
+
+    string firstName = "john";
+    string lastName = "Doe";
+    string fullName = firstName + " "  +lastName;
+    cout << fullName;
+    return 0;
+}
